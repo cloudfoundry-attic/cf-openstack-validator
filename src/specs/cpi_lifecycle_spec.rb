3dde9dd25fe3d49f10e02e2c6157bf8e113af53f
Beyhan Veli <beyhan.veli@sap.com>
2016-05-20 16:03:33 +0200

    Extract CPI handling into cpi_spec_helper
    [#118647651](https://www.pivotaltracker.com/story/show/118647651)
    Signed-off-by: Mauro Morales <mamorales@suse.com>

--- src/specs/cpi_lifecycle_spec.rb
+++ src/specs/cpi_lifecycle_spec.rb
@@ -1,12 +1,10 @@
-require 'logger'
-require 'common/common'
-require 'cloud'
 require 'ostruct'
 require 'psych'
 require 'json'
 require 'rspec/core'
 require 'yaml'
 
+require_relative 'cpi_spec_helper'
 
 def with_cpi(error_message)
   yield if block_given?
@@ -17,76 +15,40 @@ end
 describe 'Your OpenStack' do
 
   before(:all) {
-    @stemcell_path     = ENV['BOSH_OPENSTACK_STEMCELL_PATH']
-    @cpi_path          = ENV['BOSH_OPENSTACK_CPI_PATH']
-    @validator_options = YAML.load_file(ENV['BOSH_OPENSTACK_CPI_CONFIG'])['validator']
-    @log_path          = ENV['BOSH_OPENSTACK_CPI_LOG_PATH']
+    @stemcell_path     = stemcell_path
+    @cpi_path          = cpi_path
+    @validator_options = validator_options
+    @log_path          = log_path
 
     _, @server_thread = create_server
-    cpi
+    @cpi = cpi(@cpi_path, @log_path)
   }
 
   after(:all) {
-    Thread.kill(@server_thread)
+    kill_server(@server_thread)
   }
 
   after(:all) {
     if @@vm_cid_with_floating_ip
       with_cpi("VM '#{@@vm_cid_with_floating_ip}' could not be deleted.") {
-        cpi.delete_vm(@@vm_cid_with_floating_ip)
+        @cpi.delete_vm(@@vm_cid_with_floating_ip)
       }
     end
   }
 
-  let(:network_spec) do
-    {
-        'default' => {
-            'type' => 'dynamic',
-            'cloud_properties' => {
-                'net_id' => @validator_options['network_id']
-            }
-        }
-    }
-  end
-
-  let(:network_spec_with_floating_ip) do
-    {
-        'default' => {
-            'type' => 'dynamic',
-            'cloud_properties' => {
-                'net_id' => @validator_options['network_id']
-            }
-        },
-        'vip' => {
-          'type' => 'vip',
-          'ip' => @validator_options['floating_ip'],
-        }
-    }
-  end
-
-  def cpi
-    @cpi ||= begin
-      # TODO cpi log should go to $temp_dir/logs
-      Bosh::Clouds::Config.configure(OpenStruct.new(:logger => Logger.new(STDERR), :cpi_task_log => "#{@log_path}/cpi.log"))
-
-      Bosh::Clouds::ExternalCpi.new(@cpi_path, 'director-UUID')
-    end
-  end
-
   it 'can save a stemcell' do
     stemcell_manifest = Psych.load_file(File.join(@stemcell_path, "stemcell.MF"))
-    @@stemcell_cid = with_cpi('Stemcell could not be uploaded') {
-      cpi.create_stemcell(File.join(@stemcell_path, "image"), stemcell_manifest["cloud_properties"])
+    self.stemcell_cid = with_cpi('Stemcell could not be uploaded') {
+      @cpi.create_stemcell(File.join(@stemcell_path, "image"), stemcell_manifest["cloud_properties"])
     }
-
-    expect(@@stemcell_cid).to be
+    expect(stemcell_cid).to be
   end
 
   it 'can create a VM' do
     @@vm_cid = with_cpi("VM could not be created.") {
-      cpi.create_vm(
+      @cpi.create_vm(
           'agent-id',
-          @@stemcell_cid,
+          stemcell_cid,
           {'instance_type' => 'm1.small'},
           network_spec,
           [],
@@ -99,7 +61,7 @@ describe 'Your OpenStack' do
 
   it 'has vm cid' do
     with_cpi('VM cid could not be found.') {
-      @@has_vm = cpi.has_vm?(@@vm_cid)
+      @@has_vm = @cpi.has_vm?(@@vm_cid)
     }
 
     expect(@@has_vm).to be true
@@ -107,7 +69,7 @@ describe 'Your OpenStack' do
 
   it 'can create a disk' do
     @@disk_cid = with_cpi('Disk could not be created.') {
-      cpi.create_disk(2048, {}, @@vm_cid)
+      @cpi.create_disk(2048, {}, @@vm_cid)
     }
 
     expect(@@disk_cid).to be
@@ -115,7 +77,7 @@ describe 'Your OpenStack' do
 
   it 'has disk cid' do
     with_cpi('Disk cid could not be found.') {
-      @@has_disk = cpi.has_disk?(@@disk_cid)
+      @@has_disk = @cpi.has_disk?(@@disk_cid)
     }
 
     expect(@@has_disk).to be true
@@ -123,45 +85,45 @@ describe 'Your OpenStack' do
 
   it 'can attach the disk to the vm' do
     with_cpi("Disk '#{@@disk_cid}' could not be attached to VM '#{@@vm_cid}'.") {
-      cpi.attach_disk(@@vm_cid, @@disk_cid)
+      @cpi.attach_disk(@@vm_cid, @@disk_cid)
     }
   end
 
   it 'can detach the disk from the VM' do
     with_cpi("Disk '#{@@disk_cid}' could not be detached from VM '#{@@vm_cid}'.") {
-      cpi.detach_disk(@@vm_cid, @@disk_cid)
+      @cpi.detach_disk(@@vm_cid, @@disk_cid)
     }
   end
 
   it 'can take a snapshot' do
     @@snapshot_cid = with_cpi("Snapshot for disk '#{@@disk_cid}' could not be taken.") {
-      cpi.snapshot_disk(@@disk_cid, {})
+      @cpi.snapshot_disk(@@disk_cid, {})
     }
   end
 
   it 'can delete a snapshot' do
     with_cpi("Snapshot '#{@@snapshot_cid}' for disk '#{@@disk_cid}' could not be deleted.") {
-      cpi.delete_snapshot(@@snapshot_cid)
+      @cpi.delete_snapshot(@@snapshot_cid)
     }
   end
 
   it 'can delete the disk' do
     with_cpi("Disk '#{@@disk_cid}' could not be deleted.") {
-      cpi.delete_disk(@@disk_cid)
+      @cpi.delete_disk(@@disk_cid)
     }
   end
 
   it 'can delete the VM' do
     with_cpi("VM '#{@@vm_cid}' could not be deleted.") {
-      cpi.delete_vm(@@vm_cid)
+      @cpi.delete_vm(@@vm_cid)
     }
   end
 
   it 'can attach floating IP to a VM' do
     @@vm_cid_with_floating_ip = with_cpi("Floating IP could not be attached.") {
-      cpi.create_vm(
+      @cpi.create_vm(
         'agent-id',
-        @@stemcell_cid,
+        stemcell_cid,
         { 'instance_type' => 'm1.small' },
         network_spec_with_floating_ip,
         [],
@@ -190,56 +152,17 @@ describe 'Your OpenStack' do
     large_disk_cid = with_cpi("Large disk could not be created.\n" +
         'Hint: If you are using DevStack, you need to manually set a' +
         'larger backing file size in your localrc.') {
-      cpi.create_disk(30720, {})
+      @cpi.create_disk(30720, {})
     }
 
     with_cpi("Large disk '#{large_disk_cid}' could not be deleted.") {
-      cpi.delete_disk(large_disk_cid)
+      @cpi.delete_disk(large_disk_cid)
     }
   end
 
   it 'can delete a stemcell' do
     with_cpi('Stemcell could not be deleted') {
-      cpi.delete_stemcell(@@stemcell_cid)
+      @cpi.delete_stemcell(stemcell_cid)
     }
   end
-end
-
-def private_key_path
-  private_key_name = @validator_options["private_key_name"]
-  File.join(File.dirname(__FILE__), "..", "..", private_key_name)
-end
-
-def execute_ssh_command_on_vm(private_key_path, ip, command)
-  `ssh -o StrictHostKeyChecking=no -i #{private_key_path} vcap@#{ip} -C "#{command}"`
-end
-
-def create_server
-  require 'socket'
-  # TODO fake registry port should not be hard coded
-  server = TCPServer.new('localhost', 11111)
-
-  accept_thread = Thread.new {
-    loop do
-      Thread.start(server.accept) do |socket|
-        request = socket.gets
-        response = "{\"settings\":\"{}\"}\n"
-        headers = create_headers [
-                                     'HTTP/1.1 200 Ok',
-                                     'Content-Type: application/json',
-                                     "Content-Length: #{response.bytesize}",
-                                     'Connection: close']
-        socket.print headers
-        socket.print "\r\n"
-        socket.print response
-        socket.close
-      end
-    end
-  }
-
-  [server, accept_thread]
-end
-
-def create_headers(headers)
-  headers.map { |line| "#{line}\r\n" }.join('')
 end
---
+++ src/specs/cpi_spec_helper.rb
@@ -0,0 +1,102 @@
+require 'logger'
+require 'common/common'
+require 'cloud'
+
+def stemcell_cid=(stemcell_cid)
+  @@stemcell_cid = stemcell_cid
+end
+
+def stemcell_cid
+  @@stemcell_cid
+end
+
+def stemcell_path
+  ENV['BOSH_OPENSTACK_STEMCELL_PATH']
+end
+
+def cpi_path
+  ENV['BOSH_OPENSTACK_CPI_PATH']
+end
+
+def validator_options
+  @validator_options ||= YAML.load_file(ENV['BOSH_OPENSTACK_CPI_CONFIG'])['validator']
+end
+
+def log_path
+  ENV['BOSH_OPENSTACK_CPI_LOG_PATH']
+end
+
+def private_key_path
+  private_key_name = validator_options["private_key_name"]
+  File.join(File.dirname(__FILE__), "..", "..", private_key_name)
+end
+
+def execute_ssh_command_on_vm(private_key_path, ip, command)
+  `ssh -o StrictHostKeyChecking=no -i #{private_key_path} vcap@#{ip} -C "#{command}"`
+end
+
+def network_spec
+  {
+      'default' => {
+          'type' => 'dynamic',
+          'cloud_properties' => {
+              'net_id' => validator_options['network_id']
+          }
+      }
+  }
+end
+
+def network_spec_with_floating_ip
+  {
+      'default' => {
+          'type' => 'dynamic',
+          'cloud_properties' => {
+              'net_id' => validator_options['network_id']
+          }
+      },
+      'vip' => {
+          'type' => 'vip',
+          'ip' => validator_options['floating_ip'],
+      }
+  }
+end
+
+def cpi(cpi_path, log_path)
+  Bosh::Clouds::Config.configure(OpenStruct.new(:logger => Logger.new(STDERR), :cpi_task_log => "#{log_path}/cpi.log"))
+
+  Bosh::Clouds::ExternalCpi.new(cpi_path, 'director-UUID')
+end
+
+def create_server
+  require 'socket'
+  # TODO fake registry port should not be hard coded
+  server = TCPServer.new('localhost', 11111)
+
+  accept_thread = Thread.new {
+    loop do
+      Thread.start(server.accept) do |socket|
+        request = socket.gets
+        response = "{\"settings\":\"{}\"}\n"
+        headers = create_headers [
+                                     'HTTP/1.1 200 Ok',
+                                     'Content-Type: application/json',
+                                     "Content-Length: #{response.bytesize}",
+                                     'Connection: close']
+        socket.print headers
+        socket.print "\r\n"
+        socket.print response
+        socket.close
+      end
+    end
+  }
+
+  [server, accept_thread]
+end
+
+def create_headers(headers)
+  headers.map { |line| "#{line}\r\n" }.join('')
+end
+
+def kill_server(server_thread)
+  Thread.kill(server_thread)
+end
