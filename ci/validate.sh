6c553e6d25cb8b4caa6dee748101f3feb0983540
Felix Riegger <felix.riegger@sap.com>
2016-06-13 16:00:06 +0200

    Add concourse CI pipeline
    - initial validation script based on official ubuntu
      docker image installing all tools to build ruby and
      execute the rspec tests
    - for now with fixed cpi and stemcell version
    
    [#121388681](https://www.pivotaltracker.com/story/show/121388681)
    
    Signed-off-by: Beyhan Veli <beyhan.veli@sap.com>

---
+++ ci/validate.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+set -e -x
+
+: ${AUTH_URL:?}
+: ${USERNAME:?}
+: ${API_KEY:?}
+: ${DOMAIN:?}
+: ${PROJECT:?}
+: ${DEFAULT_KEY_NAME:?}
+: ${NETWORK_ID:?}
+: ${FLOATING_IP:?}
+: ${PRIVATE_KEY:?}
+
+sudo apt-get update
+sudo apt-get -y install wget make gcc zlib1g-dev libssl-dev ssh # zlibc
+
+wget -O cpi.tgz http://bosh.io/d/github.com/cloudfoundry-incubator/bosh-openstack-cpi-release?v=25
+wget -O stemcell.tgz https://d26ekeud912fhb.cloudfront.net/bosh-stemcell/openstack/bosh-stemcell-3232.6-openstack-kvm-ubuntu-trusty-go_agent.tgz
+
+echo $PRIVATE_KEY > validator-src/cf-validator.rsa_id
+
+cat > cpi.json <<EOF
+{
+  "cloud": {
+    "plugin": "openstack",
+    "properties": {
+      "openstack": {
+        "auth_url": "$AUTH_URL",
+        "username": "$USERNAME",
+        "api_key": "$API_KEY",
+        "domain": "$DOMAIN",
+        "project": "$PROJECT",
+        "default_key_name": "$DEFAULT_KEY_NAME",
+        "wait_resource_poll_interval": 5,
+        "ignore_server_availability_zone": false,
+        "endpoint_type": "publicURL",
+        "state_timeout": 300,
+        "stemcell_public_visibility": false,
+        "connection_options": {
+          "ssl_verify_peer": false
+        },
+        "boot_from_volume": false,
+        "use_dhcp": true,
+        "human_readable_vm_names": true
+      },
+      "registry": {
+        "endpoint": "http://localhost:11111",
+        "user": "fake",
+        "password": "fake"
+      }
+    }
+  },
+  "validator": {
+    "network_id": "$NETWORK_ID",
+    "floating_ip": "$FLOATING_IP",
+    "private_key_name": "cf-validator.rsa_id"
+  }
+}
+EOF
+
+pushd validator-src
+./validate ../cpi.tgz ../stemcell.tgz ../cpi.json
