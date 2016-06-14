c1b5200f286d3b010317dd66e022aa94a1502417
Felix Riegger <felix.riegger@sap.com>
2016-06-14 14:51:20 +0200

    Verify Cinder and Glance availability properly
    [#118825389](https://www.pivotaltracker.com/story/show/118825389)
    
    Signed-off-by: Beyhan Veli <beyhan.veli@sap.com>

--- src/specs/openstack_api_spec.rb
+++ src/specs/openstack_api_spec.rb
@@ -26,16 +26,16 @@ describe 'Your OpenStack' do
 
     it 'v1 for Cinder is supported' do
       begin
-        Fog::Image::OpenStack::V1.new(@openstack_params)
-      rescue Fog::OpenStack::Errors::ServiceUnavailable => e
+        Fog::Volume::OpenStack::V1.new(@openstack_params)
+      rescue Fog::Errors::NotFound => e
         fail("Your Cinder version is not supported. Supported version is 'v1'. OpenStack error: #{e.message}")
       end
     end
 
     it 'v1 for Glance is supported' do
       begin
-        Fog::Volume::OpenStack::V1.new(@openstack_params)
-      rescue Fog::OpenStack::Errors::ServiceUnavailable => e
+        Fog::Image::OpenStack::V1.new(@openstack_params)
+      rescue Fog::Errors::NotFound => e
         fail("Your Glance version is not supported. Supported version is 'v1'. OpenStack error: #{e.message}")
       end
     end
