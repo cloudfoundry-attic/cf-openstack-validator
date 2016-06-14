fc36eda896cef2f4c22f889c405ad13569f4e08b
Beyhan Veli <beyhan.veli@sap.com>
2016-06-14 10:42:51 +0200

    Render private key properly
    [#121388681](https://www.pivotaltracker.com/story/show/121388681)
    
    Signed-off-by: Felix Riegger <felix.riegger@sap.com>

--- ci/validate.sh
+++ ci/validate.sh
@@ -17,7 +17,7 @@ sudo apt-get -y install wget make gcc zlib1g-dev libssl-dev ssh # zlibc
 wget -O cpi.tgz http://bosh.io/d/github.com/cloudfoundry-incubator/bosh-openstack-cpi-release?v=25
 wget -O stemcell.tgz https://d26ekeud912fhb.cloudfront.net/bosh-stemcell/openstack/bosh-stemcell-3232.6-openstack-kvm-ubuntu-trusty-go_agent.tgz
 
-echo $PRIVATE_KEY > validator-src/cf-validator.rsa_id
+echo "${PRIVATE_KEY}" > validator-src/cf-validator.rsa_id
 chmod 400 validator-src/cf-validator.rsa_id
 
 cat > cpi.json <<EOF
