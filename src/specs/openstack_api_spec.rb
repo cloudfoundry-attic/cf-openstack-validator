5d64d3d41b44fb3d2c4ed3fc00c50cc7a59faeb7
Beyhan Veli <beyhan.veli@sap.com>
2016-08-19 14:19:06 +0200

    Remove tests on v1 of cinder and glance
    The CPI now supports both v1 and v2, so the explicit test is not needed anymore.
    
    [#128636327](https://www.pivotaltracker.com/story/show/128636327)
    
    Signed-off-by: Cornelius Schumacher <cschum@suse.com>

--- src/specs/openstack_api_spec.rb
+++ src/specs/openstack_api_spec.rb
@@ -20,25 +20,6 @@ openstack_suite.context 'API', position: 1, order: :global do
     end
   end
 
-  describe 'versions' do
-
-    it 'v1 for Cinder is supported' do
-      begin
-        Fog::Volume::OpenStack::V1.new(@fog_params)
-      rescue Fog::Errors::NotFound => e
-        fail("Your Cinder version is not supported. Supported version is 'v1'. OpenStack error: #{e.message}")
-      end
-    end
-
-    it 'v1 for Glance is supported' do
-      begin
-        Fog::Image::OpenStack::V1.new(@fog_params)
-      rescue Fog::Errors::NotFound => e
-        fail("Your Glance version is not supported. Supported version is 'v1'. OpenStack error: #{e.message}")
-      end
-    end
-  end
-
   describe 'Security groups' do
     before do
       begin
