2f239ca9d54252a659b070a2a4b7b0881fd103f9
Beyhan Veli <beyhan.veli@sap.com>
2016-11-30 09:30:55 +0100

    CI uses CPI configured in the validator configuration
    [#130620093](https://www.pivotaltracker.com/story/show/130620093)

--- ci/tasks/validate.sh
+++ ci/tasks/validate.sh
@@ -31,7 +31,7 @@ cp extensions/dummy_extension_spec.sample.rb extensions/dummy_extension_spec.rb
 
 bundle install --path .bundle
 
-bundle exec validate -r ~/cpi.tgz -s ~/stemcell.tgz -c validator.yml
+bundle exec validate -s ~/stemcell.tgz -c validator.yml
 
 CONFIG_DRIVE='disk' erb ci/assets/validator.yml.erb > validator.yml
 cat validator.yml
