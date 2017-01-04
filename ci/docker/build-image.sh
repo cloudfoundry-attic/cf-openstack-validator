349b1d9120e6f77430f57b217dd45576edd22cef
Tom Kiemes <tom.kiemes@sap.com>
2017-01-04 18:08:20 +0100

    Enable sudo for validator-ci user
    Also introduce tagging (previous, latest) for docker image
    
    [#133656167](https://www.pivotaltracker.com/story/show/133656167)

--- ci/docker/build-image.sh
+++ ci/docker/build-image.sh
@@ -6,8 +6,14 @@ DOCKER_IMAGE=${DOCKER_IMAGE:-boshcpi/cf-openstack-validator-ci}
 
 docker login
 
+echo "Download latest docker image..."
+docker pull $DOCKER_IMAGE
+
+echo "Tagging 'latest' to 'previous'..."
+docker tag $DOCKER_IMAGE $DOCKER_IMAGE:previous
+
 echo "Building docker image..."
 docker build -t $DOCKER_IMAGE .
 
-echo "Pushing docker image to '$DOCKER_IMAGE'..."
+echo "Pushing docker images to '$DOCKER_IMAGE'..."
 docker push $DOCKER_IMAGE
