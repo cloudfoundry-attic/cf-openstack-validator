2a8f25ccf990de6ebd555d9890923a7f1261bdab
Beyhan Veli <beyhan.veli@sap.com>
2017-04-11 14:00:22 +0200

    Make quota definition optional
    Also print tests skipped if no quotas found
    
    [#137608951](https://www.pivotaltracker.com/story/show/137608951)
    
    Signed-off-by: Tom Kiemes <tom.kiemes@sap.com>

--- extensions/quotas/quotas_spec.rb
+++ extensions/quotas/quotas_spec.rb
@@ -1,38 +1,56 @@
 describe 'Quotas' do
 
   config = Validator::Api.configuration.extensions
-  quotas = YAML.load_file( config['quotas']['expected_quotas'])
+  loaded_quotas = YAML.load_file(config['quotas']['expected_quotas'])
+  quotas = loaded_quotas ? loaded_quotas : {}
 
   let(:project_id) { config['quotas']['project_id'] }
-  let(:compute_quota) do
-    Validator::Api::FogOpenStack.compute.get_quota(project_id).body['quota_set']
-  end
-  let(:volume_quota) do
-    Validator::Api::FogOpenStack.volume.get_quota(project_id).body['quota_set']
-  end
-  let(:network_quota) do
-    Validator::Api::FogOpenStack.network.get_quota(project_id).body['quota']
-  end
 
-  quotas['compute'].each do |key, value|
-    it key do
-      os_quota = compute_quota[key]
-      expect(os_quota == -1 || os_quota >= value).to eq(true), "Quota for '#{key}' should be greater than '#{value}', but is '#{os_quota}'"
+  unless loaded_quotas
+    it 'executes quota tests' do
+      skip("No quota expectation defined in #{config['quotas']['expected_quotas']}")
     end
   end
 
-  quotas['volume'].each do |key, value|
-    it key do
-      os_quota = volume_quota[key]
-      expect(os_quota == -1 || os_quota >= value).to eq(true), "Quota for '#{key}' should be greater than '#{value}', but is '#{os_quota}'"
+  context 'compute' do
+    let(:compute_quota) do
+      Validator::Api::FogOpenStack.compute.get_quota(project_id).body['quota_set']
+    end
+    compute_quotas = quotas['compute'] ? quotas['compute'] : []
+
+    compute_quotas.each do |key, value|
+      it key do
+        os_quota = compute_quota[key]
+        expect(os_quota == -1 || os_quota >= value).to eq(true), "Quota for '#{key}' should be greater than '#{value}', but is '#{os_quota}'"
+      end
     end
   end
 
-  quotas['network'].each do |key, value|
-    it key do
-      os_quota = network_quota[key]
-      expect(os_quota == -1 || os_quota >= value).to eq(true), "Quota for '#{key}' should be greater than '#{value}', but is '#{os_quota}'"
+  context 'volume' do
+    let(:volume_quota) do
+      Validator::Api::FogOpenStack.volume.get_quota(project_id).body['quota_set']
+    end
+    volume_quotas = quotas['volume'] ? quotas['volume'] : []
+
+    volume_quotas.each do |key, value|
+      it key do
+        os_quota = volume_quota[key]
+        expect(os_quota == -1 || os_quota >= value).to eq(true), "Quota for '#{key}' should be greater than '#{value}', but is '#{os_quota}'"
+      end
     end
   end
 
+  context 'network' do
+    let(:network_quota) do
+      Validator::Api::FogOpenStack.network.get_quota(project_id).body['quota']
+    end
+    network_quotas = quotas['network'] ? quotas['network'] : []
+
+    network_quotas.each do |key, value|
+      it key do
+        os_quota = network_quota[key]
+        expect(os_quota == -1 || os_quota >= value).to eq(true), "Quota for '#{key}' should be greater than '#{value}', but is '#{os_quota}'"
+      end
+    end
+  end
 end
