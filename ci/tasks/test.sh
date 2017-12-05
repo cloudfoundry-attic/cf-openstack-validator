3fe9eade7fc59247eb1655492fef98fe40c3916f
Felix Riegger <felix.riegger@sap.com>
2017-12-05 16:05:02 +0100

    Use vendored gems also in test task
    [#152785196](https://www.pivotaltracker.com/story/show/152785196)
    
    Signed-off-by: Tom Kiemes <tom.kiemes@sap.com>

--- ci/tasks/test.sh
+++ ci/tasks/test.sh
@@ -2,6 +2,6 @@
 set -e
 
 pushd validator-src
-bundle install
+BUNDLE_CACHE_PATH="vendor/package" bundle install --local --deployment --path .bundle
 bundle exec rspec ci/assets/config_renderer/
 bundle exec rspec spec/
