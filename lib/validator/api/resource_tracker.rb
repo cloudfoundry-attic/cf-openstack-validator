28a77e5fea41d154d07288444119150852cfdce0
Mario Manno <mario.manno@gmail.com>
2018-06-11 15:16:53 +0200

    Do not clean up servers
    so that we can jump onto them and test traceroute

--- lib/validator/api/resource_tracker.rb
+++ lib/validator/api/resource_tracker.rb
@@ -215,6 +215,7 @@ module Validator
       end
 
       def cleanup
+        sleep 3000
         resources.map do |resource|
           RESOURCE_HANDLER.fetch(resource[:type], Base.new).destroy(resource[:type], resource[:id])
         end.all?
