f00d414804c581c3f59c26030c86bde1bfe4f0ce
Beyhan Veli <beyhan.veli@sap.com>
2018-07-13 10:56:00 +0200

    inspect
    Co-authored-by: David Ansari <david.ansari@sap.com>

--- src/specs/spec_helper.rb
+++ src/specs/spec_helper.rb
@@ -15,7 +15,7 @@ RSpec.configure do |config|
   config.validator_resources = Validator::Resources.new
 
   config.after(:all) do
-    puts ">>> after.all, config=#{config}"
+    puts ">>> after.all, config=#{config.inspect}"
     RSpec::configuration.validator_resources.cleanup unless RSpec::configuration.options.skip_cleanup?
   end
 end
